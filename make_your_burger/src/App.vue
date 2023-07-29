9dce2108e8379903968282dd5584b22b0722f49e
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-07-28 23:28:52 -0300

    Adicionando navbar e footer.

--- make_your_burger/src/App.vue
+++ make_your_burger/src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Navbar />
+    <router-view/>
+    <Footer />
+  </div>
+    
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
 
