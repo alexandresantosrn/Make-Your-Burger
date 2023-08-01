4290bdc1f668a993ccd1e44ecadecba747dffd04
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-08-01 13:49:22 -0300

    Criando formulário.

--- make_your_burger/src/views/HomeView.vue
+++ make_your_burger/src/views/HomeView.vue
@@ -2,19 +2,21 @@
   
   <div class="main-container">
     <Banner />
-    <h1>Monte o seu burguer:</h1>
-    <p>FOrmulário</p>
+    <h1>Monte o seu burger:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 
 import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 </script>
