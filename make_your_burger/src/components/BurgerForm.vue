5d4de0eb44346f2f09df36a0e295fb159fa84774
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-08-03 23:55:42 -0300

    Implementando cadastro do hamburger.

--- make_your_burger/src/components/BurgerForm.vue
+++ make_your_burger/src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Componente de Mensagem</p>
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit="createBurger">
                 <div class="input-container">
                     <label for="nome">Nome do cliente:</label>
                     <input type="text" id="nome" v-model="nome" placeholder="Digite o seu nome">
@@ -61,6 +61,27 @@ export default {
             this.paes = data.paes;
             this.carnes = data.carnes;
             this.opcionaisdata = data.opcionais;           
+        },
+        async createBurger(e) {
+            e.preventDefault();
+
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: Array.from(this.opcionais),
+                status: this.status
+            }
+
+            const dataJson = JSON.stringify(data);
+            const req = await fetch("http://localhost:3000/burgers", {
+                method: "POST",
+                headers: {"Content-type": "application-joson"},
+                body: dataJson
+            });     
+            
+            const res = await req.json();
+            console.log(res);
         }
     },
     mounted() {
