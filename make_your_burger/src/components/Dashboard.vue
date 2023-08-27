1f97f44f8ff861d04a8e3e920df93aa8cd8e00c2
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-08-27 15:58:13 -0300

    Adicionando status dos pedidos.

--- make_your_burger/src/components/Dashboard.vue
+++ make_your_burger/src/components/Dashboard.vue
@@ -22,6 +22,7 @@
                 <div>
                     <select name="status" class="status">
                         <option value="">Selecione</option>
+                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo"> {{ s.tipo }}</option> 
                     </select>
                     <button class="delete-btn">Cancelar</button>
                 </div>                   
@@ -48,6 +49,16 @@ export default {
             this.burgers = data;
 
             console.log(this.burgers);
+
+            this.getStatus();
+        },
+        async getStatus() {
+
+            const req = await fetch('http://localhost:3000/status');
+            const data = await req.json();
+            this.status = data;
+
+            console.log(this.status);
         }
     },
     mounted() {
